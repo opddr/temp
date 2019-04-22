08a47214a7d1b324bb1a5f4b733d2145013c3d7e
gilgil <gilgil@gilgil.net>
2019-04-22 18:47:03 +0900

    Update tshark-command.sh

--- tshark-command.sh
+++ tshark-command.sh
@@ -1,2 +1,14 @@
-tshark -i eth0 -T fields -e http.host -e http.request.full_uri -e urlencoded-form.value -Y "tcp contains \"11111111\""
+INTERFACE=$1
+
+function usage() {
+	echo "usage : tshark-command.sh <interface>"
+	echo "exam  : tshark-command.sh wlan0"
+}
+
+if [ -z "$INTERFACE" ]; then
+	usage
+	exit 1
+fi
+
+tshark -i "$INTERFACE" -T fields -e http.host -e http.request.full_uri -e urlencoded-form.value -Y "tcp contains \"11111111\""
 
