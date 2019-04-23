3300ca796bdd8dfd73f1d77513e8c981fd43f97d
c0msherl0ck <47681651+c0msherl0ck@users.noreply.github.com>
2019-04-23 12:21:29 +0900

    Create login_test.py

---
+++ pyscript/login_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\sungsoo.s.kim\\Documents\\#RPA\\web-driver\\chromedriver.exe')
+
+driver.implicitly_wait(3)
+
+# 고려대학교 로그인 페이지
+driver.get('https://korea.ac.kr/cop/member/memberLoginForm.do?siteId=university&id=university_090100000000')
+
+driver.find_element_by_name('userId').send_keys('testtest')
+driver.find_element_by_name('userPw').send_keys('11111111')
+
+driver.find_element_by_xpath("//div[@class='login_box']//button[@type='submit']").click()
